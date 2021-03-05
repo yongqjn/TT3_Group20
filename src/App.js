a5ab3013633a0434a3e671d3a41f125723ca0e0d
nicholasyqj@gmail.com <nicholasyqj@gmail.com>
2021-03-05 13:17:12 +0800

    user information complete with api call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import "./App.css";
 import Detailform from "./components/Detailform";
 import Login from "./components/Login";
-import React from 'react';
+import React, {useState} from 'react';
 import UserInformation from "./components/UserInformation"
 
 function App() {
-
+  const [accountKey, setaccountKey] = useState();
   const [isLoggedIn, setLoggedIn] = React.useState(false)
 
   return (
     <div>
       <h1>Welcome to DBS Seed</h1>
-      <UserInformation/>
       {!isLoggedIn ? 
       <Login 
         checkLogin={setLoggedIn}
         isLoggedIn = {isLoggedIn}
-      /> : <Detailform />}
+      /> : <UserInformation setaccountKey = {setaccountKey} accountKey = {accountKey}/>}
     </div>
   );
 }
--- src/components/UserInformation.js
+++ src/components/UserInformation.js
@@ -1,35 +1,55 @@
-import React, {useEffect} from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
+const UserInformation = ({accountKey, setaccountKey}) => {
+  
+  const [address, setaddress] = useState();
+  const [email, setemail] = useState();
+  const [firstName, setfirstName] = useState();
+  const [lastName, setlastName] = useState();
+  const [nric, setnric] = useState();
+  const [phoneNumber, setphoneNumber] = useState();
+  const [userName, setuserName] = useState();
 
-const user_info = {
-    Headers: {
-        x-api-key: 
-    },
-    Request:{
-        
-    }
+  const axios = require("axios");
 
-}
-const UserInformation = ({accountKey, firstName, lastName, nric, address, phoneNumber, email}) => {
+  async function makeRequest() {
+    const config = {
+      method: "post",
+      url:
+        "https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login",
+      headers: { "x-api-key": "jWw6Vw4WxKaVmy5YPAvz2fAEraoq9Ds6IiqRxXd5" },
+      data: JSON.stringify({
+        username: "Group20",
+        password: "IF8v6jPuIsQFJ7i",
+      }),
+    };
 
-    useEffect(() => {
-        axios.post("https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/login", 
-        return () => {
-            cleanup
-        }
-    }, [input])
-    return (
-        <div>
-            <ul >{`Account Key: ${accountKey}`}</ul>
-            <ul >{`First Name:  ${firstName}`}</ul>
-            <ul >{`Last Name: ${lastName}`}</ul>
-            <ul >{`NRIC:  ${nric}`}</ul>
-            <ul >{`Address:  ${address}`}</ul>
-            <ul >{`Phone Number:  ${phoneNumber}`}</ul>
-            <ul >{`Email:  ${email}`}</ul>
-        </div>
-    )
-}
+    let res = await axios(config);
+    console.log(res.data);
+    setaccountKey(res.data.accountKey);
+    setaddress(res.data.address);
+    setemail(res.data.email);
+    setfirstName(res.data.firstName);
+    setlastName(res.data.lastName);
+    setnric(res.data.nric);
+    setphoneNumber(res.data.phoneNumber);
+    setuserName(res.data.userName);
+  }
 
-export default UserInformation
+  makeRequest();
+
+  return (
+    <div>
+        <h1>User Information</h1>
+      <ul>{`First Name:  ${firstName}`}</ul>
+      <ul>{`Last Name: ${lastName}`}</ul>
+      <ul>{`NRIC:  ${nric}`}</ul>
+      <ul>{`Address:  ${address}`}</ul>
+      <ul>{`Phone Number:  ${phoneNumber}`}</ul>
+      <ul>{`Email:  ${email}`}</ul>
+    </div>
+  );
+};
+
+export default UserInformation;
